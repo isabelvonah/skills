0a6dc241726c5880d4f161e36df8e453fcef67e8
Isabel von Ah <isabelvonah@gmail.com>
2022-03-28 20:47:24 +0200

    add company and place and shuffle csv writing

--- 01_scraping/bs_testing.py
+++ 01_scraping/bs_testing.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from time import strftime
 import time
 import random
+from jinja2 import Undefined
 import requests
 from bs4 import BeautifulSoup
 
@@ -16,15 +17,21 @@ def get_url(page):
 
 def get_job(article, page, cat, subcat):
     atag = article.a
+    spans = article.find_all("span", "Span-sc-1ybanni-0 Text__span-sc-1lu7urs-8 Text-sc-1lu7urs-9 cAoBYw eubypz")
 
     id = str(atag.get('href'))[21:57]
     title = atag.get('title')
+    company = spans[0].get_text()
+    try:
+        place = spans[1].get_text()
+    except:
+        place = ""
     promo = False
     if str(atag.get('href')).find('promo') != -1:
         promo = True
     today = datetime.today(),strftime('%y-%m-%d')
-    
-    job = (id, cat, subcat, page, title, promo, today)
+
+    job = (id, cat, subcat, page, title, company, place, promo, today)
     return job
 
 url = get_url(0)
@@ -35,12 +42,16 @@ soup = BeautifulSoup(response.text, 'lxml')
 navigation_span = soup.find("span", "Span-sc-1ybanni-0 Text__span-sc-1lu7urs-8 Text-sc-1lu7urs-9 fJEZEL")
 number_of_pages = int(str(navigation_span)[-11:-7].replace("/",""))
 
-jobs = []
+#with open('urls_4.csv', 'w', newline='', encoding='utf-8') as f:
+   # writer = csv.writer(f)
+   # writer.writerow(['id', 'cat', 'subcat', 'page', 'title', 'company', 'place', 'promo', 'date'])
 
 # for i in range(number_of_pages):
-for i in range(3):
+for i in range(number_of_pages):
     
-    page = i + 1
+    jobs = []
+
+    page = i + 4
     url = get_url(i + 1)
 
     response = requests.get(url)
@@ -54,10 +65,9 @@ for i in range(3):
         #time.sleep(random.randint(0,9))
         #print("looped")
 
-with open('urls_3.csv', 'w', newline='', encoding='utf-8') as f:
-    writer = csv.writer(f)
-    writer.writerow(['id', 'cat', 'subcat', 'page', 'title', 'promo', 'date'])
-    writer.writerows(jobs)
+    with open('urls_4.csv', 'a', newline='', encoding='utf-8') as f:
+        writer = csv.writer(f)
+        writer.writerows(jobs)
 
 
 
