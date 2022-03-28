8e4708f777dc6355962e8e184cf4f7ef427c4b82
Isabel von Ah <isabelvonah@gmail.com>
2022-03-28 21:04:48 +0200

    add comments

--- 01_scraping/bs_testing.py
+++ 01_scraping/bs_testing.py
@@ -3,37 +3,49 @@ from datetime import datetime
 from time import strftime
 import time
 import random
-from jinja2 import Undefined
 import requests
 from bs4 import BeautifulSoup
 
+# These categories serve as examples. Eventually, a function should iterate over a list of categories and modify the url accordingly
 cat = "information-technology-telecom"
 subcat = "database-specialists-development"
 
+# forms the url
 def get_url(page):
     template = "https://www.jobs.ch/en/vacancies/{}/{}/?page={}term="
     url = template.format(cat, subcat, page)
     return url
 
+# returns a tuple of job attributes
+# article represents one result of a job search page
 def get_job(article, page, cat, subcat):
-    atag = article.a
-    spans = article.find_all("span", "Span-sc-1ybanni-0 Text__span-sc-1lu7urs-8 Text-sc-1lu7urs-9 cAoBYw eubypz")
 
+    # The a-tag links to a url including the unique id of the job offer and also deliver the title of the offer
+    # promotioned offers contain "promo"
+    atag = article.a
     id = str(atag.get('href'))[21:57]
     title = atag.get('title')
+    promo = False
+    if str(atag.get('href')).find('promo') != -1:
+        promo = True
+
+    # The company and the place are stored in a span-tag with a special class
+    # there are missing places
+    spans = article.find_all("span", "Span-sc-1ybanni-0 Text__span-sc-1lu7urs-8 Text-sc-1lu7urs-9 cAoBYw eubypz")    
     company = spans[0].get_text()
     try:
         place = spans[1].get_text()
     except:
         place = ""
-    promo = False
-    if str(atag.get('href')).find('promo') != -1:
-        promo = True
+    
+    # The date of the request is also required
     today = datetime.today(),strftime('%y-%m-%d')
 
+    # all attributes are stored in a tuple
     job = (id, cat, subcat, page, title, company, place, promo, today)
     return job
 
+# The first page gets loaded to detect the number of pages
 url = get_url(0)
 
 response = requests.get(url)
@@ -42,23 +54,25 @@ soup = BeautifulSoup(response.text, 'lxml')
 navigation_span = soup.find("span", "Span-sc-1ybanni-0 Text__span-sc-1lu7urs-8 Text-sc-1lu7urs-9 fJEZEL")
 number_of_pages = int(str(navigation_span)[-11:-7].replace("/",""))
 
-#with open('urls_4.csv', 'w', newline='', encoding='utf-8') as f:
-   # writer = csv.writer(f)
-   # writer.writerow(['id', 'cat', 'subcat', 'page', 'title', 'company', 'place', 'promo', 'date'])
+# The csv-file for the results gets created and prepared with the title row
+# must be commented out when adding additional rows instead of creating a new file
+with open('urls_4.csv', 'w', newline='', encoding='utf-8') as f:
+   writer = csv.writer(f)
+   writer.writerow(['id', 'cat', 'subcat', 'page', 'title', 'company', 'place', 'promo', 'date'])
 
-# for i in range(number_of_pages):
+# for every page of the results gets analysed with BeautifulSoup and written (appended) into the created csv-file 
 for i in range(number_of_pages):
     
     jobs = []
-
-    page = i + 4
-    url = get_url(i + 1)
+    page = i + 1
+    url = get_url(page)
 
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'lxml')
 
     articles = soup.find_all('article')
 
+    # goes through all articles (job offers)
     for article in articles:
         job = get_job(article, page, cat, subcat)
         jobs.append(job)
