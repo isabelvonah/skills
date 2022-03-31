ce68397121485cf0af4b0105691e54a193ab075c
Isabel von Ah <isabelvonah@gmail.com>
2022-03-31 12:30:34 +0200

    add industry to metas

--- 01_scraping/scrape_meta.py
+++ 01_scraping/scrape_meta.py
@@ -10,16 +10,17 @@ from bs4 import BeautifulSoup
 cat = "information-technology-telecom"
 subcat = "database-specialists-development"
 employment_typ = "5"
+industry = "4"
 
 # forms the url
 def get_url(page):
-    template = "https://www.jobs.ch/en/vacancies/{}/{}/??employment-type={}&page={}term="
-    url = template.format(cat, subcat, employment_typ, page)
+    template = "https://www.jobs.ch/en/vacancies/{}/{}/??employment-type={}&industry={}&page={}term="
+    url = template.format(cat, subcat, employment_typ, industry, page)
     return url
 
 # returns a tuple of job attributes
 # article represents one result of a job search page
-def get_job(article, page, cat, subcat, employment_typ):
+def get_job(article, page, cat, subcat, employment_typ, industry):
 
     # The a-tag links to a url including the unique id of the job offer and also deliver the title of the offer
     # promotioned offers contain "promo"
@@ -43,7 +44,7 @@ def get_job(article, page, cat, subcat, employment_typ):
     today = datetime.today(),strftime('%y-%m-%d')
 
     # all attributes are stored in a tuple
-    job = (id, cat, subcat, employment_typ, page, title, company, place, promo, today)
+    job = (id, cat, subcat, employment_typ, industry, page, title, company, place, promo, today)
     return job
 
 # The first page gets loaded to detect the number of pages
@@ -52,14 +53,17 @@ url = get_url(0)
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
 
-navigation_span = soup.find("span", "Span-sc-1ybanni-0 Text__span-sc-1lu7urs-8 Text-sc-1lu7urs-9 fJEZEL")
-number_of_pages = int(str(navigation_span)[-11:-7].replace("/",""))
+navigation_span = soup.find_all("span", "Span-sc-1ybanni-0 Text__span-sc-1lu7urs-8 Text-sc-1lu7urs-9 fJEZEL")
+if len(navigation_span) == 0:
+    number_of_pages = 1
+else:
+    number_of_pages = int(str(navigation_span[0])[-11:-7].replace("/",""))
 
 # The csv-file for the results gets created and prepared with the title row
 # must be commented out when adding additional rows instead of creating a new file
 with open('urls_5.csv', 'w', newline='', encoding='utf-8') as f:
    writer = csv.writer(f)
-   writer.writerow(['id', 'cat', 'subcat', 'employment-typ', 'page', 'title', 'company', 'place', 'promo', 'date'])
+   writer.writerow(['id', 'cat', 'subcat', 'employment-typ', 'industry', 'page', 'title', 'company', 'place', 'promo', 'date'])
 
 # for every page of the results gets analysed with BeautifulSoup and written (appended) into the created csv-file 
 for i in range(number_of_pages):
@@ -75,7 +79,7 @@ for i in range(number_of_pages):
 
     # goes through all articles (job offers)
     for article in articles:
-        job = get_job(article, page, cat, subcat, employment_typ)
+        job = get_job(article, page, cat, subcat, employment_typ, industry)
         jobs.append(job)
         #time.sleep(random.randint(0,9))
         #print("looped")
