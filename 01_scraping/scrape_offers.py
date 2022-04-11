fd678d896c4ed5da8e141940bf4de9798a2361aa
Isabel von Ah <isabelvonah@gmail.com>
2022-04-11 16:07:52 +0200

    modify scrape_offers for csv-access and iteration

--- 01_scraping/scrape_offers.py
+++ 01_scraping/scrape_offers.py
@@ -8,8 +8,12 @@ from inscriptis import get_text
 from bs4 import BeautifulSoup
 
 
-# These id's serve as examples. Eventually, a function should iterate over a list of id's and modify the url accordingly
-ids = ["b28c5f6c-8af3-4325-927f-0a06a9105e75", "32289881-3fa2-47e2-b9c4-d66348aa099e", "a1513ed6-9763-4618-a866-1527f6dd17c6", "6a24f7ee-fd7a-4246-bf62-57b384d609a9"]
+ids = []
+
+with open("results/result_marketing-communications-editorial_online-marketing-social-media.csv", 'r', newline='', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            ids.append(row[0])
 
 # forms the url
 def get_url(id):
@@ -17,18 +21,8 @@ def get_url(id):
     url = template.format(id)
     return url
 
-# The csv-file for the results gets created and prepared with the title row
-# must be commented out when adding additional rows instead of creating a new file
-with open('offers.csv', 'w', newline='', encoding='utf-8') as f:
-   writer = csv.writer(f)
-   writer.writerow(['id', 'title', 'date', 'company', 'place', 'position', 'workload', 'categories', 'text'])
-
-for i in range(len(ids)):
-
-    url = get_url(ids[i])
-
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
+# returns a tuple of job attributes
+def get_offer(soup):
 
     title_div = soup.find("h1")
     title = title_div.get("title")
@@ -47,12 +41,37 @@ for i in range(len(ids)):
   
     offer_div = soup.find("div", "Div-sc-1cpunnt-0 DetailVacancyView__StyledVacancyDetailWrapper-sc-8f0fxs-0 jzJSph gpGrF")
     iframe = offer_div.div
+    # uses inscriptis instead of Beautiful Soup
     text = get_text(str(iframe))
 
-    offer = (ids[i], title, date, company, place, position, workload, categories, text)
+    # The date of the request is also required as a log attribute
+    today = datetime.today(),strftime('%y-%m-%d')
+
+    offer = (ids[i], title, date, company, place, position, workload, categories, text, today)
+    return offer
+
+# The csv-file for the results gets created and prepared with the title row
+with open('results/03_scrape_offers.csv', 'w', newline='', encoding='utf-8') as f:
+   writer = csv.writer(f)
+   writer.writerow(['id', 'title', 'date', 'company', 'place', 'position', 'workload', 'categories', 'text', 'today'])
+
+for i in range(len(ids)):
+
+    url = get_url(ids[i])
+
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    # to slow the algorithm down and don't send too many requests at once
+    time.sleep(random.randint(0,5))
+
+    try:
+        offer = get_offer(soup)
     
-    with open('offers.csv', 'a', newline='', encoding='utf-8') as f:
-        writer = csv.writer(f)
-        writer.writerow(offer)
+        with open('results/03_scrape_offers.csv', 'a', newline='', encoding='utf-8') as f:
+            writer = csv.writer(f)
+            writer.writerow(offer)
+
+    except: pass
 
 
