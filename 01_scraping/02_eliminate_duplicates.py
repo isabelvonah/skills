8859fe1ec56f250c2a1c5dd581a487a6fe6a095b
Isabel von Ah <isabelvonah@gmail.com>
2022-04-14 12:04:33 +0200

    eliminate duplicates

--- 01_scraping/02_eliminate_duplicates.py
+++ 01_scraping/02_eliminate_duplicates.py
@@ -7,5 +7,9 @@ import os
 files = os.path.join("01_scrape_meta", "result*.csv")
 files = glob.glob(files)
 
-df = pd.concat(map(pd.read_csv, files), ignore_index=True) 
+df = pd.concat(map(pd.read_csv, files), ignore_index=True)
+print(df.info())
+df.drop_duplicates(subset="id", inplace=True)
+print(df.info())
+print(df.value_counts("id"))
 df.to_csv('02_eliminate_duplicates.csv', index=False) 
