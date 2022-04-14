d95456360bf22d9c18705067a4b8fc4abdacc724
Isabel von Ah <isabelvonah@gmail.com>
2022-04-14 12:00:44 +0200

    prepare concatination of result files

--- 01_scraping/02_eliminate_duplicates.py
+++ 01_scraping/02_eliminate_duplicates.py
@@ -4,9 +4,8 @@ import os
 
 # tutorial: https://www.tutorialspoint.com/how-to-merge-all-csv-files-into-a-single-dataframe-python-pandas
 
-files = os.path.join("~/Documents/github/skills/01_scraping/01_scrape_meta", "result*.csv")
-
+files = os.path.join("01_scrape_meta", "result*.csv")
 files = glob.glob(files)
 
 df = pd.concat(map(pd.read_csv, files), ignore_index=True) 
-print(df)
+df.to_csv('02_eliminate_duplicates.csv', index=False) 
